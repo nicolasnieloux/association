51b685d2f1e0d259fcc5b4582c0fb897a19b36da
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-08-08 09:10:06 +0200

    Add DateToBrushConverter and update project files

---
+++ associationWpf/Converter/DateToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace associationWpf.Converter
+{
+    public class DateToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date && parameter is DateTime startDate)
+            {
+                return date == startDate ? Brushes.Red : Brushes.Black;
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
